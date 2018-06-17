8e8754669a16c5d7937eaf385d090507c294260e
Wenceslaus Chalyi <34477181+MiraculousNightingale@users.noreply.github.com>
2018-06-17 19:43:28 +0300

    Delete db_config.php

--- src/common/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Set up a configuration for your own server.
-
-define('DB_SERVER', 'localhost:8889');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'stargazer_db');
